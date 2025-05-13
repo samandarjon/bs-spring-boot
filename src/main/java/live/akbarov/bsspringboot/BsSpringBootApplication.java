e9cbc44f01b6bb6285b0602e8476d82e3748a8d4
samandar_akbarov <samandar_akbarov@epam.com>
2025-05-13 17:00:12 +0500

    Big bang

---
+++ src/main/java/live/akbarov/bsspringboot/BsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package live.akbarov.bsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BsSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/live/akbarov/bsspringboot/BsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package live.akbarov.bsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
